52ab399d20817b8e29f1c127fceef2b58cbf9f4a
Nguyễn Tuấn Việt <127472325+vietnt02@users.noreply.github.com>
2023-08-12 01:37:26 +0700

    Delete Find the Runner-Up Score.py

--- Python/Introduction/Find the Runner-Up Score.py
+++
@@ -1,6 +0,0 @@
-# https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
-if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-arr = sorted(list(set(arr)),reverse = True)
-print(arr[1])
